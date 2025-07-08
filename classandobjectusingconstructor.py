b12f1353c2c1ad1d5636db86019df4b4f218b351
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-07-09 01:27:04 +0530

    object initialization using init method in python

---
+++ classandobjectusingconstructor.py
@@ -0,0 +1,12 @@
+class Human:
+    def __init__(self,name):
+        self.name = name
+
+    def my_name(self):
+        print("my name is {}".format(self.name))
+
+h1=Human("Akshay")
+h1.my_name()
+
+h2=Human("sayali")
+h2.my_name()
